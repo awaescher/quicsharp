4016a729c91044ebe35cbeb6bb9481b7ff192f5b
Andreas Wäscher <awaescher@gmx.de>
2018-01-17 00:00:43 +0100

    Comment lines out instead of removing them to keep the line numbers in sync

--- quic#.Engine/CodePreparer.cs
+++ quic#.Engine/CodePreparer.cs
@@ -10,19 +10,26 @@ namespace quicsharp.Engine
 			var result = new PreparationResult();
 
 			var lines = code.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-			var newLines = new List<string>();
 
-			foreach (var line in lines)
+			for (int i = 0; i < lines.Length; i++)
 			{
+				var line = lines[i];
+
+				// Note: Comment lines out instead of removing them to keep the line numbers the same
+				//		 for the user and the compiler. This is important for error messages with line numbers.
 				if (line.StartsWith("#i"))
+				{
 					result.Imports.Add(line.Substring(3));
+					lines[i] = "//" + line;
+				}
 				else if (line.StartsWith("#r"))
+				{
 					result.References.Add(line.Substring(3));
-				else
-					newLines.Add(line);
+					lines[i] = "//" + line;
+				}
 			}
 
-			result.Code = string.Join(Environment.NewLine, newLines);
+			result.Code = string.Join(Environment.NewLine, lines);
 
 			return result;
 		}
