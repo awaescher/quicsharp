646d61aff1b39d5f2c829134aaaa26a12c209595
Andreas Wäscher <awaescher@gmx.de>
2018-01-30 23:46:40 +0100

    Add a nuspec file to create a nuget package for the engine

--- quicsharp.Engine/Properties/AssemblyInfo.cs
+++ quicsharp.Engine/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("quicsharp.Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple C# scripting engine without dependencies.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andreas Wäscher")]
 [assembly: AssemblyProduct("quicsharp.Engine")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Andreas Wäscher")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
